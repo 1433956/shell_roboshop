a478f79eae4f44a72b871b3724d5630e5503236f
raghu <raghumurukutla96@gmail.com>
2025-05-20 22:09:50 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -78,12 +78,10 @@ VALIDATE $? "Starting Catalogue"
  dnf install mongodb-mongosh -y &>>$LOG_FILE
  VALIDATE $? "Installing MongoDB Client"
  STATUS=$(mongosh --host mongodb.devops73.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-
-    if [ $STATUS -lt 0 ]
-    then
-        mongosh --host mongodb.devops73.site </app/db/master-data.js &>>$LOG_FILE
-        VALIDATE $? "Loading data into MongoDB"
-
-    else
-        echo -e "Data is already loaded ... $Y SKIPPING $N"
-    fi
+  if [ $STATUS -lt 0 ]
+  then
+       mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+       VALIDATE $? "Loading data into MongoDB"
+  else
+       echo -e "Data is already loaded ... $Y SKIPPING $N"
+  fi

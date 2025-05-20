1f1d638f69cf58da0f8987b1b6414eec4d38e482
raghu <raghumurukutla96@gmail.com>
2025-05-20 22:04:57 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -78,10 +78,10 @@ VALIDATE $? "Starting Catalogue"
  dnf install mongodb-mongosh -y &>>$LOG_FILE
  VALIDATE $? "Installing MongoDB Client"
  STATUS=$(mongosh --host mongodb.devops73.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
- 
+
 if [ $STATUS -lt 0 ]
 then
-     mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+     mongosh --host mongodb.devops73.site </app/db/master-data.js &>>$LOG_FILE
      VALIDATE $? "Loading data into MongoDB"
 
 else
